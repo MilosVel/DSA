c9edf4c7f39218f69d7ae7eadef551146a0355c6
milos <milos@novustaffing.com>
2025-03-05 16:04:01 +0100

    builder

---
+++ design-patterns/builder/dir-scraper-class.ts
@@ -0,0 +1,45 @@
+import fs from "fs";
+
+interface IFileReader {
+  isJSONFile(filePath: string): boolean;
+  readTextFile(filePath: string): string;
+  readJSONFile(filePath: string): unknown;
+}
+
+class DirectoryScraper {
+  constructor(public dirPath: string, public reader: IFileReader) { }
+
+  scanFiles() {
+    return fs
+      .readdirSync(this.dirPath)
+      .reduce<Record<string, unknown>>((a, fileName) => {
+        if (this.reader.isJSONFile(fileName)) {
+          a[fileName] = this.reader.readJSONFile(`${this.dirPath}/${fileName}`);
+        } else {
+          a[fileName] = this.reader.readTextFile(`${this.dirPath}/${fileName}`);
+        }
+        return a;
+      }, {});
+  }
+}
+
+class FileReader implements IFileReader {
+  isJSONFile(filePath: string): boolean {
+    return filePath.endsWith(".json");
+  }
+  readTextFile(filePath: string): string {
+    return fs.readFileSync(filePath, "utf8").toString();
+  }
+  readJSONFile(filePath: string): unknown {
+    return JSON.parse(fs.readFileSync(filePath, "utf8").toString());
+  }
+}
+
+const ds = new DirectoryScraper("./data", new FileReader());
+console.log(ds.scanFiles());
+
+
+// {
+//   'json-file.json': { name: 'Luke Skywalker', properti: true },
+//   'text-file.txt': "Luke Skywalker is a Jedi\nAnd that's pretty cool."
+// }
