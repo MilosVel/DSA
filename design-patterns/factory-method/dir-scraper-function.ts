710490316660ac771ca1da1152e53e3ecdc8cc8f
milos <milos@novustaffing.com>
2025-03-05 16:08:26 +0100

    factory method

---
+++ design-patterns/factory-method/dir-scraper-function.ts
@@ -0,0 +1,35 @@
+import fs from "fs";
+
+interface IFileReader {
+  isJSONFile(filePath: string): boolean;
+  readTextFile(filePath: string): string;
+  readJSONFile(filePath: string): unknown;
+}
+
+const createDirScanner = (reader: IFileReader) => {
+  return (dirPath: string) =>
+    fs.readdirSync(dirPath).reduce<Record<string, unknown>>((a, fileName) => {
+      if (reader.isJSONFile(fileName)) {
+        a[fileName] = reader.readJSONFile(`${dirPath}/${fileName}`);
+      } else {
+        a[fileName] = reader.readTextFile(`${dirPath}/${fileName}`);
+      }
+      return a;
+    }, {});
+};
+
+const scanFiles = createDirScanner({
+  isJSONFile: (filePath: string): boolean => filePath.endsWith(".json"),
+  readTextFile: (filePath: string): string =>
+    fs.readFileSync(filePath, "utf8").toString(),
+  readJSONFile: (filePath: string): unknown =>
+    JSON.parse(fs.readFileSync(filePath, "utf8").toString()),
+});
+
+console.log(scanFiles("./data"));
+
+
+// {
+//   'json-file.json': { name: 'Luke Skywalker', jedi: true },
+//   'text-file.txt': "Luke Skywalker is a Jedi\nAnd that's pretty cool."
+// }
