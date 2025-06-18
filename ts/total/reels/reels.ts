a8075d8397d056e09e008cd96b5d486143b0a915
Milos <milosvelickovic1992@gmail.com>
2025-06-18 08:35:27 +0200

    ts magic

---
+++ ts/total/reels/reels.ts
@@ -0,0 +1,60 @@
+////////////////     TIP 3 Prettify
+
+
+type ComplexType = {
+    a: string;
+    b: string;
+} & Omit<{
+    c: boolean;
+} & Record<'d', number[]>, 'c'>
+
+
+type Prettify<T> = {
+    [K in keyof T]: T[K]
+} & {}   // ovo je uobicajeni nastavak za Prettify & {}
+
+
+type ShowMe = Prettify<ComplexType>
+
+// type ShowMe = {
+//   a: string;
+//   b: string;
+//   d: number[];
+// }
+
+
+////////////////////////////////////////////////////////
+
+type ToDiscoUnion<T extends Record<string, object>
+> = {
+  [K in keyof T]: Prettify<
+    {
+      type: K;
+    } & T[K]
+  >;
+}[keyof T];
+
+type State = ToDiscoUnion<{
+  loading: {};
+  error: {
+    message: string;
+  };
+  complete: {
+    name: string;
+  };
+}>;
+
+
+
+// type State =
+//   | {
+//       type: 'loading';
+//     }
+//   | {
+//       type: 'error';
+//       message: string;
+//     }
+//   | {
+//       type: 'complete';
+//       name: string;
+//     };
