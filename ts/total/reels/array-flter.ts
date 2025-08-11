17116e0b9ba68f2e723ae6d529d8b10500e47259
Milos <milosvelickovic1992@gmail.com>
2025-08-11 13:11:48 +0200

    explanation

--- ts/total/reels/array-flter.ts
+++ ts/total/reels/array-flter.ts
@@ -13,4 +13,14 @@ type ResultNumber = ExcludeFromArray<Arr,string> // type ResultNumber = number[]
 
 type helper = Arr[number] // type helper = string | number
 
+type helper2 = Exclude<helper, number> // type helper2 = string
+type helper3 = Exclude<helper, string> // type helper3 = number
+
+///////////////////  basic helper
+
+type basicHelper = number | string
+type basicHelper2 = Exclude<basicHelper, number> // type basicHelper2 = string
+type basicHelper3 = Exclude<basicHelper, string> // type basicHelper3 = number
+
+
 
