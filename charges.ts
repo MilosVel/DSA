a456fdebbe66dc1d11efacf212d297ac0f206513
Milos <milosvelickovic1992@gmail.com>
2025-08-14 11:08:04 +0200

    save

--- charges.ts
+++ charges.ts
@@ -153,23 +153,23 @@ console.log(groupedCharges?.outlet_member_position_price);
 
 
 // {
-//     outlet_position_price: {
-//       current: {
-//         hourly_charge: 33,
-//         dates: [
-//           '2025-08-12',
-//           '2025-08-13',
-//           '2025-08-14',
-//           '2025-08-16',
-//           '2025-08-21',
-//           '2025-08-31',
-//           '2025-09-06'
-//         ]
-//       }
-//     },
-//     special_outlet_position_price: {
-//       current: { hourly_charge: 33, dates: [ '2025-08-25', '2025-08-28' ] },
-//       future: { hourly_charge: 35, dates: [ '2025-08-29' ] }
+//   outlet_position_price: {
+//     current: {
+//       hourly_charge: 33,
+//       dates: [
+//         '2025-08-12',
+//         '2025-08-13',
+//         '2025-08-14',
+//         '2025-08-16',
+//         '2025-08-21',
+//         '2025-08-31',
+//         '2025-09-06'
+//       ]
 //     }
+//   },
+//   special_outlet_position_price: {
+//     current: { hourly_charge: 33, dates: [ '2025-08-25', '2025-08-28' ] },
+//     future: { hourly_charge: 35, dates: [ '2025-08-29' ] }
 //   }
-//   undefined
+// }
+// undefined
