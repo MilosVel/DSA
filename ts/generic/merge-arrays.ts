e675cbe2fc382ef5a38a384c0ab8a799ee6d9768
Milos Vel <milosvelickovic1992@gmail.com>
2025-09-26 19:09:05 +0200

    merge array by keys function

--- ts/generic/merge-arrays.ts
+++ ts/generic/merge-arrays.ts
@@ -9,14 +9,14 @@
 //     key2: keyof T2,
 //   ): (T1 & T2)[] {
 //     const map = new Map<number, T1 & T2>();
-  
+
 //     array1.forEach((item) => {
 //       const key = item[key1];
 //       if (typeof key === 'number') {
 //         map.set(key, structuredClone(item) as T1 & T2);
 //       }
 //     });
-  
+
 //     array2.forEach((item) => {
 //       const key = item[key2];
 //       if (typeof key === 'number') {
@@ -28,10 +28,10 @@
 //         }
 //       }
 //     });
-  
+
 //     return Array.from(map.values());
 //   }
-  
+
 
 
 
@@ -39,15 +39,15 @@
 //     { id: 1, name: "Alice", age: 25 },
 //     { id: 2, name: "Bob", age: 30 },
 //   ];
-  
+
 //   const orders = [
 //     { userId: 1, orderTotal: 100 },
 //     { userId: 2, orderTotal: 150 },
 //     { userId: 3, orderTotal: 200 }, // This user does not exist in `users`
 //   ];
-  
+
 //   const mergedData = mergeArraysByKeys(users, orders, "id", "userId");
-  
+
 //   console.log(mergedData);
 
 // //   [
@@ -65,28 +65,29 @@
 
 import merge from 'lodash.merge';
 
-export function mergeArraysByKeys<T1 extends Record<string, any>, T2 extends Record<string, any>>(
+export function mergeArraysByKeys<T1, T2>(
   array1: T1[],
   array2: T2[],
   key1: keyof T1,
-  key2: keyof T2
+  key2: keyof T2,
 ): (T1 & T2)[] {
-  const map = new Map<string | number, T1 & T2>();
+  const map = new Map<number, T1 & T2>();
 
   array1.forEach((item) => {
     const key = item[key1];
-    if (typeof key === 'string' || typeof key === 'number') {
-      map.set(key, { ...item } as T1 & T2);
+    if (typeof key === 'number') {
+      map.set(key, structuredClone(item) as T1 & T2);
     }
   });
 
   array2.forEach((item) => {
     const key = item[key2];
-    if (typeof key === 'string' || typeof key === 'number') {
-      if (map.has(key)) {
-        merge(map.get(key), item);
+    if (typeof key === 'number') {
+      const existing = map.get(key);
+      if (existing) {
+        map.set(key, merge(structuredClone(existing), structuredClone(item)));
       } else {
-        map.set(key, { ...item } as T1 & T2);
+        map.set(key, structuredClone(item) as T1 & T2);
       }
     }
   });
